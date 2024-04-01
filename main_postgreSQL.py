0db36e46fc1d45398e194a33a0312c17980fb2d1
kasyan1337 <tristokorun@gmail.com>
2024-04-01 11:42:31 +0800

    Coursework - Amendments

--- main_postgreSQL.py
+++ main_postgreSQL.py
@@ -24,7 +24,7 @@ for company in russian_it_companies_default:
 # Preparing environment for using the DBManager and filtering vacancies
 db_manager = DBManager.DBManager(conn_params)
 
-# DBManager usage
+# DBManager example usage
 with DBManager.DBManager(conn_params) as manager:
     # print(manager.get_companies_and_vacancies_count()) # Showing vacancies and counting them by company
     # print(manager.get_all_vacancies()) # Showing all vacancies test
